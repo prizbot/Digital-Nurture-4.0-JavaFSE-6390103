647316ab579751953a34d4f7044bd79276143579
priya <130780101+prizbot@users.noreply.github.com>
2025-08-11 21:51:08 +0530

    Update scenario01.sql

--- Deep Skilling/Week 2 - PL SQL/Exercise 7 - Packages/scenario01.sql
+++ Deep Skilling/Week 2 - PL SQL/Exercise 7 - Packages/scenario01.sql
@@ -5,3 +5,33 @@ CREATE OR REPLACE PACKAGE CustomerManagement AS
     FUNCTION GetCustomerBalance(p_id NUMBER) RETURN NUMBER;
 END CustomerManagement;
 /
+
+-- Package Body
+CREATE OR REPLACE PACKAGE BODY CustomerManagement AS
+
+    PROCEDURE AddCustomer(p_id NUMBER, p_name VARCHAR2, p_balance NUMBER) IS
+    BEGIN
+        INSERT INTO Customers (CustomerID, CustomerName, Balance)
+        VALUES (p_id, p_name, p_balance);
+    END AddCustomer;
+
+    PROCEDURE UpdateCustomer(p_id NUMBER, p_name VARCHAR2, p_balance NUMBER) IS
+    BEGIN
+        UPDATE Customers
+        SET CustomerName = p_name,
+            Balance = p_balance
+        WHERE CustomerID = p_id;
+    END UpdateCustomer;
+
+    FUNCTION GetCustomerBalance(p_id NUMBER) RETURN NUMBER IS
+        v_balance NUMBER;
+    BEGIN
+        SELECT Balance INTO v_balance FROM Customers WHERE CustomerID = p_id;
+        RETURN v_balance;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RETURN 0;
+    END GetCustomerBalance;
+
+END CustomerManagement;
+/
